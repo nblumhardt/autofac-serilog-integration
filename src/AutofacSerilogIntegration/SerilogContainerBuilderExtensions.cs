fcabe2ce36b67c8cde8ceffae54046944982a23e
Art Ganify <artganify@gmail.com>
2016-02-05 13:23:57 +0100

    Added callback parameter to change how ILogger is being registered

--- src/AutofacSerilogIntegration/SerilogContainerBuilderExtensions.cs
+++ src/AutofacSerilogIntegration/SerilogContainerBuilderExtensions.cs
@@ -5,6 +5,8 @@ using Serilog;
 
 namespace AutofacSerilogIntegration
 {
+    using Autofac.Builder;
+
     /// <summary>
     /// Extends <see cref="ContainerBuilder"/> with registration methods for Serilog logging.
     /// </summary>
@@ -18,11 +20,12 @@ namespace AutofacSerilogIntegration
         /// <param name="logger">The logger. If null, the static <see cref="Log.Logger"/> will be used.</param>
         /// <param name="autowireProperties">If true, properties on reflection-based components of type <see cref="ILogger"/> will
         /// be injected.</param>
+        /// <param name="configureRegistration">Allows to additionally configure how <see cref="ILogger"/> is being registered within the container.</param>
         /// <returns>An object supporting method chaining.</returns>
-        public static IModuleRegistrar RegisterLogger(this ContainerBuilder builder, ILogger logger = null, bool autowireProperties = false)
+        public static IModuleRegistrar RegisterLogger(this ContainerBuilder builder, ILogger logger = null, bool autowireProperties = false, Action<IRegistrationBuilder<ILogger, SimpleActivatorData, SingleRegistrationStyle>> configureRegistration = null)
         {
             if (builder == null) throw new ArgumentNullException("builder");
-            return builder.RegisterModule(new ContextualLoggingModule(logger, autowireProperties));
+            return builder.RegisterModule(new ContextualLoggingModule(logger, autowireProperties, configureRegistration));
         }
     }
 }
